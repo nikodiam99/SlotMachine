c085761ce05ed230ed48d01f46981c32fdc8be07
Niko <nikodiam99@gmail.com>
2023-04-28 18:09:51 -0400

    prints out the columns correctly

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ MIN_BET = 1
 
 ROWS = 3
 COLS = 3
- # Dictionary
+# Dictionary
 symbol_count = {
     "A": 2,
     "B": 4,
@@ -15,19 +15,50 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    for line in range(lines):
+        symbol = columns[0][line]
+
+
 def get_slotmachine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
-        for _ in range(symbol_count): # underscore is used instead of i if you dont care about counter to avoid unused variable.
+        for _ in range(
+                symbol_count):  # underscore is used instead of i if you dont care about counter to avoid unused variable.
             all_symbols.append(symbol)
 
     columns = []
-    for col in range(cols):
+    for _ in range(cols):
         column = []
-        current_symbols = all_symbols[:] # the colon inside bracket is to make a copy of all symbols, instead of a reference of the same object.
-        for row in range(rows):
-            value = random.choice(all_symbols)
-    
+        current_symbols = all_symbols[
+                          :]  # the colon inside bracket is to make a copy of all symbols, instead of a reference of the same object.
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")  # what end does is it tells print statement when to end the line
+            else:
+                print(column[row], end="")
+
+        print()
 
 
 def deposit():
@@ -38,11 +69,13 @@ def deposit():
             if amount >= 0:
                 break
             else:
-                print ("Amount must be greater than 0.")
+                print("Amount must be greater than 0.")
         else:
             print("Please enter a number.")
 
     return amount
+
+
 # helooo
 def get_number_of_lines():
     while True:
@@ -58,12 +91,13 @@ def get_number_of_lines():
 
     return lines
 
+
 def get_bet():
     while True:
         amount = input("How much would you like to bet on each line? $")
         if amount.isdigit():
             amount = int(amount)
-            if MIN_BET<= amount <= MAX_BET:
+            if MIN_BET <= amount <= MAX_BET:
                 break
             else:
                 print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
@@ -72,19 +106,23 @@ def get_bet():
 
     return amount
 
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
-        total_bet = bet* lines
+        total_bet = bet * lines
 
         if total_bet > balance:
             print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
         else:
             break
-    
+
     print(f"You are betting ${bet} on {lines} lines.Total bet equals to: ${total_bet}")
+
+    slots = get_slotmachine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
